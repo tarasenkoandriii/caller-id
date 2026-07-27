99ed83fce728c57c4b36508257d39aa42ecc3ecb
MacBook <macbook@MACATARASENKO-84998.local>
2026-07-27 13:33:13 +0300

    deps invocation fix

--- backend/src/client-campaigns/client-campaigns.module.ts
+++ backend/src/client-campaigns/client-campaigns.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientAuthModule } from '../client-auth/client-auth.module';
 import { CallsModule } from '../calls/calls.module';
 import { VoiceoverModule } from '../voiceover/voiceover.module';
@@ -8,7 +10,18 @@ import { ClientVoiceoverController } from './client-voiceover.controller';
 import { CronController } from './cron.controller';
 
 @Module({
-  imports: [ClientAuthModule, CallsModule, VoiceoverModule],
+  imports: [
+    ClientAuthModule,
+    CallsModule,
+    VoiceoverModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+      }),
+    }),
+  ],
   controllers: [ClientCampaignsController, ClientVoiceoverController, CronController],
   providers: [ClientCampaignsService],
   exports: [ClientCampaignsService],
--- backend/src/client-contacts/client-contacts.module.ts
+++ backend/src/client-contacts/client-contacts.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientAuthModule } from '../client-auth/client-auth.module';
 import { ClientContactsService } from './client-contacts.service';
 import { ClientContactsController } from './client-contacts.controller';
 
 @Module({
-  imports: [ClientAuthModule],
+  imports: [
+    ClientAuthModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+      }),
+    }),
+  ],
   controllers: [ClientContactsController],
   providers: [ClientContactsService],
   exports: [ClientContactsService],
